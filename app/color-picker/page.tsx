0577b6e6be3c5dd1a3a2f3e291f659f57c2a5da9
kanagusam@5 <kanagarajwhb@gmail.com>
2025-04-19 16:17:03 +0530

    added color picker component functionality code

--- app/color-picker/page.tsx
+++ app/color-picker/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
+import { ColorPicker } from "@/components/ui/colorPicker"
 import { Container, Typography } from "@mui/material"
-import { Box } from "@mui/material"
 
 export default function ColorPickerPage() {
   return (
@@ -12,14 +12,7 @@ export default function ColorPickerPage() {
       <Typography variant="body1" color="text.secondary" sx={{ mb: 4 }}>
         Pick colors from images or create your own custom color schemes.
       </Typography>
-      <Box sx={{ p: 4, textAlign: "center" }}>
-        <Typography variant="h5" color="text.secondary">
-          Coming Soon
-        </Typography>
-        <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-          This tool is currently under development and will be available soon.
-        </Typography>
-      </Box>
+      <ColorPicker />
     </Container>
   )
 }
