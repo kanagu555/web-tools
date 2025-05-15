d00dfd08792173c1b8135e59c752db1061e4ac40
kanagusam@5 <kanagarajwhb@gmail.com>
2025-05-15 11:24:58 +0530

    svg icons added to tools category section

---
+++ src/data/Utils.tsx
@@ -0,0 +1,10 @@
+
+import  pdf  from '../../src/assets/pdf.svg';
+import  text  from '../../src/assets/text.svg';
+import  design  from '../../src/assets/design.svg';
+import  developer  from '../../src/assets/developer.svg';
+
+export const PdfIcon = <img src={pdf} alt="PDF Icon" width={40} height={40} />
+export const TextIcon = <img src={text} alt="Text Icon" width={40} height={40} />
+export const DesignIcon = <img src={design} alt="Design Icon" width={40} height={40} />
+export const DeveloperIcon = <img src={developer} alt="Developer Icon" width={40} height={40} />
