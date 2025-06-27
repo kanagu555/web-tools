3807eead561276ff98c678568e2c3ca798d33357
kanagusam@5 <kanagarajwhb@gmail.com>
2025-06-27 22:10:12 +0530

    fix: update PDF tools URL and alternate links in sitemap for consistency

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8be8n7ascho"
+    "revision": "0.ps0h44gtsa8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
