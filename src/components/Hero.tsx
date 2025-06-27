bdf642402fe64665bd458f5115102b3161dedf5e
kanagusam@5 <kanagarajwhb@gmail.com>
2025-06-27 22:24:48 +0530

    fix: wrap AdSense component in Box for improved layout and styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -116,21 +116,23 @@ const Hero = () => {
         }}
       />
 
-      {/* <AdSense adSlot="2209979291" width={800} height={60} /> */}
-
-      {isProductionEnv && (
-        <AdSense
-          adSlot="2209979291"
-          width="100%"
-          height={60}
-          style={{
-            maxWidth: 1140,
-            display: "block",
-            marginLeft: "auto",
-            marginRight: "auto",
-          }}
-        />
-      )}
+      <Box sx={{ position: "relative", zIndex: 1 }}>
+        {isProductionEnv && (
+          <AdSense
+            adSlot="2209979291"
+            adFormat="auto"
+            width={800}
+            height={60}
+            style={{
+              display: "inline-block",
+              width: 800,
+              height: 60,
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          />
+        )}
+      </Box>
 
       {/* Background Elements */}
       <Box
