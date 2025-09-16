d7d93ace62517fcc14cd3e93cd568fca8726439e
kanagusam@5 <kanagarajwhb@gmail.com>
2025-09-16 23:28:19 +0530

    refactor(middleware): comment out early return to apply security headers

--- middleware.ts
+++ middleware.ts
@@ -69,7 +69,7 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(url, 301);
   }
 
-  return NextResponse.next();
+  // return NextResponse.next();
 
   // Add security headers
   const response = NextResponse.next();
